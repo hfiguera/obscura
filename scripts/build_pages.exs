91fe3120f381c9c893ff4ce213b7369e6632bbb8
Humberto Figuera <hfiguera@gmail.com>
2026-09-19 15:22:22 -0600

    Add article index link above blog post titles

--- scripts/build_pages.exs
+++ scripts/build_pages.exs
@@ -284,6 +284,10 @@ defmodule Obscura.PagesBuilder do
         <a class="skip-link" href="#article">Skip to article</a>
         #{site_header("../../")}
         <main id="article" class="article-shell">
+          <a class="article-back-link" href="../../">
+            <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true" focusable="false"><path d="M19 12H5m7-7-7 7 7 7"/></svg>
+            All articles
+          </a>
           <article>#{rendered_article}</article>
         </main>
         #{site_footer("../../")}
